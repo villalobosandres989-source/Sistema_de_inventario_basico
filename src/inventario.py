562ecbfaac52ac3894dcdd7c9205b4ffc7e6e309
Camilo Villalobos <villalobosandres989@gmail.com>
2026-03-16 20:06:27 -0500

    sistema completo

--- src/inventario.py
+++ src/inventario.py
@@ -1,17 +1,18 @@
+import time
 def menu():
 
     print("=== MENU PRINCIPAL ===")
     print("1. Agregar un producto")
     print("2. Mostrar inventario")
-    print("3. Calcular estadísticas")
-    print("4. Salir")
+    print("3. Actualizar producto")
+    print("4. Eliminar producto")
+    print("5. Salir")
+
 
 inventario = []
 
-def agregar_producto():
-        nombre = input("Nombre del producto:\n")
-        precio = float(input('Ingrese el precio: '))
-        cantidad = int(input('Ingrese la cantidad: '))
+def agregar_producto(nombre, precio, cantidad):
+        
         producto = {
             'Nombre': nombre,
             'Precio': precio,
@@ -20,6 +21,60 @@ def agregar_producto():
         inventario.append(producto)
 
         print(f"Producto '{nombre}' agregado al inventario.\n")
+
+def ver_inventario():
+    if len(inventario) == 0:
+        print('El inventario está vacío')
+    else:
+        for i in inventario:
+            print(f'Nombre: {i['Nombre']} | Precio: {i['Precio']:.3f} | Cantidad: {i['Cantidad']}')
+
+
+
+def actualizar_producto():
+    nombre_buscar = input("¿Cuál producto desea actualizar?\n")
+    for producto in inventario:
+        if producto['Nombre'].lower() == nombre_buscar.lower():
+            print("Producto encontrado. Ingrese los nuevos datos.\n")
+            nuevo_nombre = input("Nuevo nombre: ")
+            nuevo_precio = float(input("Nuevo precio: "))
+            nueva_cantidad = int(input("Nueva cantidad: "))
+
+            producto['Nombre'] = nuevo_nombre
+            producto['Precio'] = nuevo_precio
+            producto['Cantidad'] = nueva_cantidad
+
+            print("Producto actualizado correctamente.\n")
+            return
+        
+    print("Producto no encontrado.\n")
+
+
+def eliminar_producto():
+
+    nombre_buscar = input("Ingrese el nombre del producto que desea eliminar:\n")
+
+    for producto in inventario:
+
+        if producto['Nombre'].lower() == nombre_buscar.lower():
+
+            inventario.remove(producto)
+
+            print("Producto eliminado correctamente.\n")
+            return
+
+    print("Producto no encontrado.\n")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -40,18 +95,30 @@ while True:
         print("Error: Por favor, ingrese un número válido.\n")
 
     if opcion == 1:
-        agregar_producto()
+        nombre = input("Nombre del producto:\n")
+        precio = float(input('Ingrese el precio: '))
+        cantidad = int(input('Ingrese la cantidad: '))
+
+        agregar_producto(nombre, precio, cantidad)
         
 
     elif opcion == 2:
-        print('INVENTARIO')
-        print(inventario)
+        ver_inventario()
         print('')
 
     elif opcion == 3:
-        print("Opcion no disponible aún\n")
+        actualizar_producto()
 
 
     elif opcion == 4:
-        print("Saliendo...")
+        eliminar_producto()
+
+    elif opcion == 5:
+        print('Saliendo...')
+        time.sleep(1)
         break
+
+
+    
+       
+    
