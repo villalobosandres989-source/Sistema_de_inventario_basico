bb64f82be75a4235528341192ee46c27dcb7af66
Camilo Villalobos <villalobosandres989@gmail.com>
2026-03-19 15:33:15 -0500

    final2

--- src/inventario.py
+++ src/inventario.py
@@ -30,17 +30,12 @@ def ver_inventario():
         for i in inventario:
             print(f'Nombre: {i['Nombre']} | Precio: {i['Precio']}$ | Cantidad: {i['Cantidad']}')
 
-def actualizar_producto(nombre_buscar, nuevo_nombre, nuevo_precio, nueva_cantidad):
-    for producto in inventario:
-        if producto['Nombre'].lower() == nombre_buscar.lower():
-            print("Producto encontrado. Ingrese los nuevos datos.\n")
-
-            producto['Nombre'] = nuevo_nombre
-            producto['Precio'] = nuevo_precio
-            producto['Cantidad'] = nueva_cantidad
-            print("Producto actualizado correctamente.\n")
-            return True
-    return False
+def actualizar_producto(producto, nuevo_nombre, nuevo_precio, nueva_cantidad):
+        producto['Nombre'] = nuevo_nombre
+        producto['Precio'] = nuevo_precio
+        producto['Cantidad'] = nueva_cantidad
+        print("Producto actualizado correctamente.\n")
+        
         
 def eliminar_producto(nombre_buscar):
     
@@ -100,53 +95,26 @@ while True:
 
     elif opcion == 3:
         
-        if len(inventario) ==0:
-            print('El inventario está vacío\n')
-            continue
-        cancelar = False
-        while True:
-            nombre_buscar = input("¿Cuál producto desea actualizar?\n")
-            existe = any(producto['Nombre'].lower() == nombre_buscar.lower() for producto in inventario)
-
-            if existe:
+        if len(inventario) == 0:
+            print('Inventario vacio\n')
+        else:
+            nombre_buscar = input('Nombre del producto que desea actualizar: ')
+        
+        producto_encontrado = None
+        for producto in inventario:
+            if producto['Nombre'].lower() == nombre_buscar.lower():
+                producto_encontrado = producto
                 break
-            else:
-                print('Producto no encontrado')
-                opcion_reintentar = input('Desea intentar de nuevo? (si/no): ').lower()
-
-                if opcion_reintentar == 'no':
-                    print('\nInventaro actual:')
-                    ver_inventario()
-                    print('')
-                    cancelar = True
-                    break
-                elif opcion_reintentar != 'si':
-                    print('opción inválida')
-        if cancelar:
-            continue
-        nuevo_nombre = input('Nuevo nombre: ')
-
-        while True:
-            try:
-                nuevo_precio = float(input('Nuevo Precio: '))
-                if nuevo_precio < 0:
-                    print('Ingrese un numero positivo')
-                else:
-                    break
-            except ValueError:
-                print('Ingrese un valor numérico')
-        while True:
-            try:
-                nueva_cantidad = int(input('Nueva Cantidad: '))
-                if nueva_cantidad < 0:
-                    print('Ingrese un número positivo')
-                else:
-                    break
-            except ValueError:
-                print('Ingrese un número entero positivo')         
+        if producto_encontrado:
+            print('Producto encontrado')
             
-        actualizar_producto(nombre_buscar, nuevo_nombre, nuevo_precio, nueva_cantidad)
-
+            nuevo_nombre = input('Nuevo nombre: ')
+            nuevo_precio = float(input('Nuevo precio: '))
+            nueva_cantidad = int(input('Nueva Cantidad: '))
+            
+            actualizar_producto(producto_encontrado, nuevo_nombre, nuevo_precio, nueva_cantidad)
+        else:
+            print('Producto no encontrado\n')
 
     elif opcion == 4:
         cancelar = False
